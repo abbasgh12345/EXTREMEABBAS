ad3a13393da62bcf7f0c7f3823a30ad065e7edcd
abbasgh12345 <abbasgh91@gmail.com>
2016-02-25 18:32:21 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,31 +1,29 @@
 do
+
  function run(msg, matches)
- 
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
+ local ch = 'chat#id'..msg.to.id
+ local fuse = '📌 NEW FEEDBACK\n\n👤 NAME: ' .. msg.from.print_name .. '\n\n👤 USERNAME: @' .. msg.from.username ..'\n\n👤 ID: ' .. msg.from.id ..'\n\n👤 GROUP ID : '..msg.to.id.. '\n\n📝 TEXT :\n\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
- 
- 
+
+
    local text = matches[1]
-   local chat = "chat#id"..158990680 
-   --like : local chat = "chat#id"..12345678
-   
+   local chat = "chat#id"..142822621
+
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'بازخورد شما ارسال شد'
- 
+  return '✅ Message successfully sent'
+
  end
  end
  return {
-  
+
   description = "Feedback",
- 
-  usage = "!feedback message",
+
+  usage = "feedback: Send A Message To Admins.",
   patterns = {
-  "^[!/][Ff]eedback (.*)$"
- 
+  "^[!/][Ff]eedback (.*)$",
+  "^[Ff]eedback (.*)$",
+  "^نظر (.*)$"
+  
   },
   run = run
  }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
